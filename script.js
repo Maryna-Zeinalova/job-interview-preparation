2a5e96573c09c4efa9656ad6acab2168954d3b3e
Maryna Zeinalova <shmapetto@gmail.com>
2023-03-13 09:06:35 +0200

    added sorting and posting new question feature

--- script.js
+++ script.js
@@ -28,35 +28,67 @@ const questions = [
     status: false,
   },
 ];
+const cardsElement = document.getElementById("cards");
+let cardsHTML = "";
 
-function displayCards() {
-  let cardsElement = document.getElementById("cards");
-  let statusHTML = "";
-  let cardsHTML = "";
-  let cardsColorHTML = "";
+function checkStatus(question) {
+  if (question.status === true) {
+    return {
+      statusHTML: `<i class="fa-solid fa-check has-text-light"></i></span><span>Got it`,
+      cardsColorHTML: "primary",
+    };
+  } else {
+    return {
+      statusHTML: `<i class="fa-solid fa-xmark has-text-light"></i></span><span>Still learning`,
+      cardsColorHTML: "warning",
+    };
+  }
+}
 
-  questions.forEach((element) => {
-    if (element.status === true) {
-      statusHTML = `<i class="fa-solid fa-check has-text-light"></i></span><span>Got it`;
-      cardsColorHTML = "primary";
-    } else {
-      statusHTML = `<i class="fa-solid fa-xmark has-text-light"></i></span><span>Still learning`;
-      cardsColorHTML = "warning";
-    }
-    cardsHTML =
-      cardsHTML +
-      `
-            <div class="column is-4 tile is-parent">
-              <div class="card tile is-child box has-background-${cardsColorHTML}">
-                <div class="card-content">
-                  <div class="icon-text has-text-grey pb-2"><span class="icon has-text-info">${statusHTML}</span></div>
-                  <p class="title is-size-5 has-text-grey">${element.question}</p>
-                </div>
+function showCards(element) {   
+  cardsHTML =
+    cardsHTML +
+    `
+          <div class="column is-4 tile is-parent">
+            <div class="card tile is-child box has-background-${checkStatus(element).cardsColorHTML}">
+              <div class="card-content">
+                <div class="icon-text has-text-grey pb-2"><span class="icon has-text-info">${checkStatus(element).statusHTML}</span></div>
+                <p class="title is-size-5 has-text-grey">${element.question}</p>
               </div>
             </div>
-`
-  });
+          </div>
+`;
+  return cardsHTML;
+}
 
+function addNewQuestion() {  
+  let newQuestion = document.querySelector(".textarea");
+  cardsElement.innerHTML = "";
+  questions.push({ question: newQuestion.value, status: false });
+  displayCards();
+}
+
+function showFalseCards() {
+  cardsHTML = "";
+  const arrayStatusFalse = questions.filter(
+    (question) => question.status === false
+  );
+  cardsElement.innerHTML = "";
+  arrayStatusFalse.forEach(showCards);
+  cardsElement.insertAdjacentHTML("afterbegin", cardsHTML);
+}
+
+function displayCards() {
+  cardsElement.innerHTML = "";
+  cardsHTML = `<div class="column is-4 tile is-parent">
+  <div class="card tile is-child box has-background-light">
+    <div class="card-content">
+      <textarea class="textarea" placeholder="Add your own question here.." id="new-question"></textarea>
+      <button class="button is-white mt-3 is-fullwidth is-primary" onclick="addNewQuestion()">Add new question</button>
+    </div>
+  </div>
+  </div>`;
+  questions.forEach(showCards);
   cardsElement.insertAdjacentHTML("afterbegin", cardsHTML);
 }
 displayCards();
