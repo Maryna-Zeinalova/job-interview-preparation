328290fbc36aa7b5cb9dd8ee3c15748cd468e276
Maryna Zeinalova <shmapetto@gmail.com>
2023-03-18 19:30:02 +0200

    created change stautus function

---
+++ questions.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+      question:
+        "What's the difference between a variable that is: null, undefined or undeclared?",
+      status: true,
+    },
+    {
+      question: "Explain how this works in JavaScript",
+      status: false,
+    },
+    {
+      question:
+        "What language constructions do you use for iterating over object properties and array items?",
+      status: true,
+    },
+    {
+      question:
+        "What are the pros and cons of extending built-in JavaScript objects?",
+      status: false,
+    },
+    {
+      question: "What is the difference between == and ===?",
+      status: true,
+    },
+    {
+      question:
+        "What is strict mode? What are some of the advantages / disadvantages of using it?",
+      status: false,
+    },
+  ];
--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-const questions = [
-  {
-    question:
-      "What's the difference between a variable that is: null, undefined or undeclared?",
-    status: true,
-  },
-  {
-    question: "Explain how this works in JavaScript",
-    status: false,
-  },
-  {
-    question:
-      "What language constructions do you use for iterating over object properties and array items?",
-    status: true,
-  },
-  {
-    question:
-      "What are the pros and cons of extending built-in JavaScript objects?",
-    status: false,
-  },
-  {
-    question: "What is the difference between == and ===?",
-    status: true,
-  },
-  {
-    question:
-      "What is strict mode? What are some of the advantages / disadvantages of using it?",
-    status: false,
-  },
-];
 const cardsElement = document.getElementById("cards");
 let cardsHTML = "";
+
+function displayCards() {
+  cardsElement.innerHTML = "";
+  cardsHTML = `
+  <div class="column is-4 tile is-parent">
+  <div class="card tile is-child box has-background-light">
+    <div class="card-content">
+      <textarea class="textarea" placeholder="Add your own question here.." id="new-question"></textarea>
+      <button class="button is-white mt-3 is-fullwidth is-primary" onclick="addNewQuestion()">Add new question</button>
+    </div>
+  </div>
+  </div>`;
+  questions.forEach(iterateCards);
+  cardsElement.insertAdjacentHTML("afterbegin", cardsHTML);
+  subscribe();
+}
+
+function iterateCards(element, index) {
+  cardsHTML =
+    cardsHTML +
+    `
+          <div class="column is-4 tile is-parent">
+            <div class="card tile is-child box has-background-${
+              checkStatus(element).cardsColorHTML
+            }">
+              <div class="card-content">
+                <div class="icon-text has-text-grey pb-2" data-question="${index}"><span class="icon has-text-info">${
+      checkStatus(element).statusHTML
+    }</span></div>
+                <p class="title is-size-5 has-text-grey">${element.question}</p>
+              </div>
+            </div>
+          </div>
+`;
+  return cardsHTML;
+}
 
 function checkStatus(question) {
   if (question.status === true) {
@@ -45,27 +51,11 @@ function checkStatus(question) {
   }
 }
 
-function iterateCards(element) {   
-  cardsHTML =
-    cardsHTML +
-    `
-          <div class="column is-4 tile is-parent">
-            <div class="card tile is-child box has-background-${checkStatus(element).cardsColorHTML}">
-              <div class="card-content">
-                <div class="icon-text has-text-grey pb-2"><span class="icon has-text-info">${checkStatus(element).statusHTML}</span></div>
-                <p class="title is-size-5 has-text-grey">${element.question}</p>
-              </div>
-            </div>
-          </div>
-`;
-  return cardsHTML;
-}
-
-function addNewQuestion() {  
-  let newQuestion = document.querySelector(".textarea");
+function addNewQuestion() {
+  const newQuestion = document.querySelector(".textarea");
   cardsElement.innerHTML = "";
   questions.push({ question: newQuestion.value, status: false });
-  displayCards();
+  displayCards(questions);
 }
 
 function showFalseCards() {
@@ -78,17 +68,23 @@ function showFalseCards() {
   cardsElement.insertAdjacentHTML("afterbegin", cardsHTML);
 }
 
-function displayCards() {
-  cardsElement.innerHTML = "";
-  cardsHTML = `<div class="column is-4 tile is-parent">
-  <div class="card tile is-child box has-background-light">
-    <div class="card-content">
-      <textarea class="textarea" placeholder="Add your own question here.." id="new-question"></textarea>
-      <button class="button is-white mt-3 is-fullwidth is-primary" onclick="addNewQuestion()">Add new question</button>
-    </div>
-  </div>
-  </div>`;
-  questions.forEach(iterateCards);
-  cardsElement.insertAdjacentHTML("afterbegin", cardsHTML);
-}
 displayCards();
+
+function subscribe() {
+  const listQuestionSelectors = document.querySelectorAll(
+    "#cards > div > div > div > div"
+  );
+  listQuestionSelectors.forEach((element) => {
+    const id = element.dataset.question;
+    element.addEventListener("click", function (event) {
+      changeStatus(id);
+    });
+  });
+}
+subscribe();
+
+function changeStatus(id) {
+  const currentQuestion = questions[id];
+  currentQuestion.status = !currentQuestion.status;
+  displayCards();
+}
